633b94923976d4510335bfb6af5865db2d93ca32
Jan Ouwens <jan.ouwens@ordina.nl>
2017-12-08 13:53:07 +0100

    Cleanup

--- src/main/scala/todo/TodoList.scala
+++ src/main/scala/todo/TodoList.scala
@@ -10,25 +10,6 @@ object TodoList extends App {
   // ??? pretty print the sequence of tasks
   tasks.foreach(println(_)) // the result will not be human-friendly
 
-  private def interpret(input: String, tasks: Seq[Task] = Seq.empty): Seq[Task] = {
-    input match {
-      case "" =>
-        tasks
-      case line =>
-        val parser = TodoParser(line)
-        parser.task.run() match {
-          case Failure(error: ParseError) =>
-            println(parser.formatError(error, new ErrorFormatter(showTraces = true)))
-            tasks
-          case Failure(exception) =>
-            println(exception)
-            tasks
-          case Success(task) =>
-            interpret(StdIn.readLine(), tasks :+ task)
-        }
-    }
-  }
-
   private def getTasks(fileName: String): Seq[Task] = {
     val lines = Source.fromResource(fileName).getLines()
 
@@ -46,8 +27,6 @@ object TodoList extends App {
         case Success(task) =>
           tasks :+ task
       }
-
     }
-
   }
 }
