921c1b0cb789df6fe55806420a04b059a88c56da
Donovan de Kuiper <donovan.dekuiper@gmail.com>
2017-12-08 13:11:58 +0100

    Changed App to read from a file instead of std

--- src/main/scala/todo/TodoList.scala
+++ src/main/scala/todo/TodoList.scala
@@ -1,12 +1,12 @@
 package todo
 
-import org.parboiled2.{ErrorFormatter, ParseError}
-
-import scala.io.StdIn
-import scala.util.{Failure, Success}
+import java.io.File
+import org.parboiled2.{ ErrorFormatter, ParseError }
+import scala.io.{ Source, StdIn }
+import scala.util.{ Failure, Success }
 
 object TodoList extends App {
-  val tasks = interpret(StdIn.readLine())
+  val tasks = getTasks("todo.txt")
   // ??? pretty print the sequence of tasks
   tasks.foreach(println(_)) // the result will not be human-friendly
 
@@ -27,5 +27,27 @@ object TodoList extends App {
             interpret(StdIn.readLine(), tasks :+ task)
         }
     }
+  }
+
+  private def getTasks(fileName: String): Seq[Task] = {
+    val lines = Source.fromResource(fileName).getLines()
+
+    lines.filter(_ != "").foldLeft(Seq.empty[Task]) {
+      case (tasks, s) =>
+      val parser = TodoParser(s)
+
+      parser.task.run() match {
+        case Failure(error: ParseError) =>
+          println(parser.formatError(error, new ErrorFormatter(showTraces = true)))
+          tasks
+        case Failure(exception) =>
+          println(exception)
+          tasks
+        case Success(task) =>
+          tasks :+ task
+      }
+
+    }
+
   }
 }
