623dda057517bf43d4bb3128d95f23b70e0cc1fe
Jan Ouwens <jan.ouwens@ordina.nl>
2018-09-28 11:59:32 +0200

    Cleans up the code a bit

--- src/main/scala/todo/TodoList.scala
+++ src/main/scala/todo/TodoList.scala
@@ -1,32 +1,34 @@
 package todo
 
-import java.io.File
-import org.parboiled2.{ ErrorFormatter, ParseError }
-import scala.io.{ Source, StdIn }
-import scala.util.{ Failure, Success }
+import org.parboiled2.{ErrorFormatter, ParseError}
 
+import scala.io.Source
+import scala.util.{Failure, Success}
+
+/**
+ * Run this program to see some non-humanfriendly output and to verify that it all works
+ */
 object TodoList extends App {
-  val tasks = getTasks("todo.txt")
-  // ??? pretty print the sequence of tasks
-  tasks.foreach(println(_)) // the result will not be human-friendly
+
+  getTasks("todo.txt").foreach(println)
 
   private def getTasks(fileName: String): Seq[Task] = {
     val lines = Source.fromResource(fileName).getLines()
 
     lines.filter(_ != "").foldLeft(Seq.empty[Task]) {
       case (tasks, s) =>
-      val parser = TodoParser(s)
+        val parser = TodoParser(s)
 
-      parser.task.run() match {
-        case Failure(error: ParseError) =>
-          println(parser.formatError(error, new ErrorFormatter(showTraces = true)))
-          tasks
-        case Failure(exception) =>
-          println(exception)
-          tasks
-        case Success(task) =>
-          tasks :+ task
-      }
+        parser.task.run() match {
+          case Failure(error: ParseError) =>
+            println(parser.formatError(error, new ErrorFormatter(showTraces = true)))
+            tasks
+          case Failure(exception) =>
+            println(exception)
+            tasks
+          case Success(task) =>
+            tasks :+ task
+        }
     }
   }
 }
